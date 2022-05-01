6b5b66e4a2128b1411b33bf8ba73ca4d99c516db
Abdullah Chaudhry <appleconda@gmail.com>
2022-05-01 20:44:05 +0500

    printing the values in mainn now

--- tst.cpp
+++ tst.cpp
@@ -5,7 +5,11 @@
 #include <sstream>
 #include <queue> 
 #include <pthread.h>
+#include <cmath>
+#include <math.h>
 using namespace std; 
+
+typedef void* (*THREADFUNCPTR) (void* ); 
 
 class task
 {
@@ -14,12 +18,80 @@ public:
     int task_value;
     int arrival_time;
     int unit_count;
-	
+	int* task_value_arr; 
     int *unitIdList;
 
 	task() 
-	{ id = 0; task_value = 0; arrival_time = 0; unit_count = 0; unitIdList = NULL;}
+	{ id = 0; task_value = 0; arrival_time = 0; unit_count = 0; unitIdList = NULL; task_value_arr = NULL;}
+	void* work_thread1()
+	{
+		for (auto i = 0; i < unit_count; i++)
+		{
+			if (unitIdList[i] == 0)
+			{
+				task_value_arr[i] = task_value; 
+				task_value_arr[i] += 5; 
+				task_value_arr[i] = task_value_arr[i] % 5000; 
+			}
+			else if (unitIdList[i] == 1)
+			{
+				task_value_arr[i] = task_value; 
+				task_value_arr[i] = task_value_arr[i] * 7; 
+				task_value_arr[i] = task_value_arr[i]%5000; 
+			}
+			else if (unitIdList[i] == 2)
+			{
+				task_value_arr[i] = task_value; 
+				double temp = task_value; 
+				double x = 5;
+				task_value_arr[i] =  pow(task_value, x); 
+				task_value_arr[i] = task_value_arr[i] % 5000; 
+			}
+			else if (unitIdList[i] == 3)
+			{
+				task_value_arr[i] = task_value; 
+				task_value_arr[i] -= 100; 
+			}
+			else if (unitIdList[i] == 4)
+			{
+				task_value_arr[i] = task_value; 
+				task_value_arr[i] = sqrt(task_value_arr[i]);
+			}
+		}
+	}
+	void* work_thread()
+	{
+		pthread_t threads[unit_count]; 
+		int rtn; 
+		cout << "inside thread" << task_value << endl; 
+		for (auto i = 0; i < unit_count; i++)
+		{
+		
+			if (rtn = pthread_create(&threads[i], NULL, (THREADFUNCPTR) &task::work_thread1, this))
+			{   
+				fprintf(stderr, "Error: Pthread_create, "); 
+				if (rtn == EAGAIN)
+					fprintf(stderr, "Insufficient resources "); 
+				else if(rtn == EINVAL)
+					fprintf (stderr, "Invalid Arguments"); 
+				exit(1); 
+			}
+           
+		}	
+		for (auto& exit : threads)
+			pthread_join(exit, NULL); 
 
+		
+	}
+
+	void print()
+	{
+		for (auto i = 0;i < unit_count; i++)
+		{
+			//cout << i << endl; 
+			cout << "Task value " << i << " " << task_value_arr[i] << endl; 
+		}
+	}
 	task operator = (const task &obj)
 	{
 		id = obj.id; 
@@ -49,6 +121,7 @@ void tokenizer(string str, task& tasks, queue<task> waiting_queue)
 	tasks.task_value = stoi(token[1]); 
 	tasks.unit_count = stoi(token[2]); 
 	tasks.unitIdList = new int[stoi(token[2])]; 
+	tasks.task_value_arr = new int[tasks.unit_count]; 
 	int count(3); 
 	
 	for (auto i = 0;i < stoi(token[2]); i++)
@@ -83,7 +156,7 @@ int main()
 		cout << i << endl ;
 		number_of_lines++;
 	}
-	cout << number_of_lines; 
+	
 	queue<task> waiting_queue; 
 	fout.close(); 
 	task* tasks = new task[number_of_lines]; 
@@ -91,5 +164,39 @@ int main()
 	{
 		tokenizer(row_data[i], tasks[i], waiting_queue);
 	}
+	//type definign here 
+
+
+
+	// Work of dispathcer start from here 
+	pthread_t pids[number_of_lines]; 
+	int rtn; 
+	task** my_task = new task*[number_of_lines]; 
+
+	for (auto i = 0; i < number_of_lines; i++)
+	{
+		my_task[i] = &tasks[i] ;
+	}
 	
+	
+
+	for (auto i = 0; i < number_of_lines; i++)
+	{
+		
+		if (rtn = pthread_create(&pids[i], NULL, (THREADFUNCPTR) &task::work_thread, my_task[i]))
+ 		{   
+			fprintf(stderr, "Error: Pthread_create, "); 
+			if (rtn == EAGAIN)
+				fprintf(stderr, "Insufficient resources "); 
+			else if(rtn == EINVAL)
+				fprintf (stderr, "Invalid Arguments"); 
+			exit(1); 
+    	}
+           
+	}
+
+	for (auto& exit : pids)
+		pthread_join(exit, NULL); 
+
+	cout << my_task[0]->task_value_arr[0] << endl; 
 }
